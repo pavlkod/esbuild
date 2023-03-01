bc192f81acfd5243b6c3d91a6350f0e95655cce9
Pavel.K <pavelweb74@gmail.com>
2023-03-01 23:18:08 +0500

    add resolvePath function

--- config/build/esbuild-config.js
+++ config/build/esbuild-config.js
@@ -4,10 +4,15 @@ const path = require("path");
 const mode = process.env.MODE || "development";
 const isDev = mode === "development";
 
+function resolvePath(...segments) {
+  return path.resolve(__dirname, "..", "..", ...segments);
+}
+
 module.exports = {
-  outdir: path.resolve(__dirname, "../../build"),
-  entryPoints: [path.resolve(__dirname, "..", "..", "src", "index.jsx")],
+  outdir: resolvePath("build"),
+  entryPoints: [resolvePath("src", "index.jsx")],
   entryNames: "bundle",
   bundle: true,
   minify: !isDev,
+  tsconfig: resolvePath("tsconfig.json"),
 };
